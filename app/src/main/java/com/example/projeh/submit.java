c74f41294f2bb6f708fc3870a135f696017ecfe7
masomeh-darvishha <arezoodarvishha@gmail.com>
2021-05-31 14:52:52 +0430

    i fix problem in manifest  add a submit ui  connect mainactivity to submit

--- app/src/main/java/com/example/projeh/submit.java
+++ app/src/main/java/com/example/projeh/submit.java
@@ -3,28 +3,23 @@ package com.example.projeh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import com.airbnb.lottie.LottieAnimationView;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    LottieAnimationView login;
+public class submit extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        login = findViewById(R.id.login);
-        login.playAnimation();
-
+        setContentView(R.layout.activity_submit);
     }
-    public void onclicke(View v) {
+
+    public void onclick(View v) {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, submit.class));
+                startActivity(new Intent(submit.this, MainActivity.class));
 
             }
         }, 1000);
